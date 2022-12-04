abce91f56b13d113085c5332953f052fd8f84a4a
herbbbbbb <herbert.bjerre@gmail.com>
2022-12-04 13:56:46 +0100

    Initial Commit

---
+++ List/ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListApp.swift
+//  List
+//
+//  Created by Herbert Lind Bjerre on 04/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
